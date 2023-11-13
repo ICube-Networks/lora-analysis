c930373e4e4a99720784bdf03fb3a3169c52ddd7
Fabrice Théoleyre <fabrice.theoleyre@cnrs.fr>
2023-11-13 17:21:19 +0100

    a single query to count with a groupby (SF distribution)

--- SF_analysis.py
+++ SF_analysis.py
@@ -4,7 +4,7 @@ import requests, json, os, tarfile, pathlib
 import myconfig
 import pandas as pd
 
-
+from elasticsearch_dsl import Search
 
 #number of results for a query
 QUERY_NB_RESULT=10000
@@ -25,60 +25,110 @@ print("ID of the elastic search connection for the query: ", pit_id)
 
 
 
+if True:
 
-# list of clients with specific SF
-numRecords = pd.Series()
-for SF in range(7,12):
+    # list of clients with specific SF
+    numRecords = pd.Series()
+    for SF in range(7,12):
 
-    #initialization with an empty value for this SF
-    if SF not in numRecords.index:
-        numRecords = pd.concat([numRecords, pd.Series(data=[0], index=[SF])])
+        #initialization with an empty value for this SF
+        if SF not in numRecords.index:
+            numRecords = pd.concat([numRecords, pd.Series(data=[0], index=[SF])])
+
+        # initially, the first date in the range is nothing (=0)
+        datemin="0"
+        while True:
+            
+            resp = clientES.search(
+                size=QUERY_NB_RESULT,
+                query={
+                    "bool": {
+                        "filter": [
+                            {"match": {"rxInfo.crcStatus": "CRC_OK"}},
+                            {"term": {"txInfo.loRaModulationInfo.spreadingFactor": SF}},
+                        ],
+                    },
+                },
+                pit={
+                    "id": pit_id,
+                    "keep_alive": "1m",
+                },
+                sort=[
+                    {"mqtt_time": {"order": "asc"}},
+                    {"_score": {"order": "desc"}},
+                ],
+                search_after=[
+                    datemin,
+                    0
+                ],
+            )
+            
+            #metadata of the response
+            length = len(resp['hits']['hits'])
+            numRecords[SF] = numRecords[SF] + length
+            #print("Got %d Hits:" % length)
+             
+            #stops if we have less than QUERY_SIZE elements
+            if (length < QUERY_NB_RESULT):
+                break
+
+            #extracts the mqtt-time of the last element for the next query
+            datemin = resp['hits']['hits'][length-1]['_source']['mqtt_time']
+             
+
+    #final results
+    print(numRecords)
+
+    
+
+    
+    
+    
+if True:
+
+    # list of clients with specific SF
+    numRecords = pd.Series()
 
     # initially, the first date in the range is nothing (=0)
     datemin="0"
-    while True:
-        
-        resp = clientES.search(
-            size=QUERY_NB_RESULT,
-            query={
-                "bool": {
-                    "filter": [
-                        {"match": {"rxInfo.crcStatus": "CRC_OK"}},
-                        {"term": {"txInfo.loRaModulationInfo.spreadingFactor": SF}},
-                    ],
-                },
-            },
-            pit={
-                "id": pit_id,
-                "keep_alive": "1m",
-            },
-            sort=[
-                {"mqtt_time": {"order": "asc"}},
-                {"_score": {"order": "desc"}},
-            ],
-            search_after=[
-                datemin,
-                0
-            ],
-        )
-        
-        #metadata of the response
-        length = len(resp['hits']['hits'])
-        numRecords[SF] = numRecords[SF] + length
-        #print("Got %d Hits:" % length)
-         
-        #stops if we have less than QUERY_SIZE elements
-        if (length < QUERY_NB_RESULT):
-            break
 
-        #extracts the mqtt-time of the last element for the next query
-        datemin = resp['hits']['hits'][length-1]['_source']['mqtt_time']
-         
-
-#final results
-print(numRecords)
+            
+    resp = clientES.search(
+        size=0,
+        query={
+            "bool": {
+                "filter": [
+                    {"match": {"rxInfo.crcStatus": "CRC_OK"}} #,
+                  #  {"term": {"txInfo.loRaModulationInfo.spreadingFactor": SF}},
+                ],
+            },
+        },
+        aggs={
+            "toto": { "terms" : { "field" : "txInfo.loRaModulationInfo.spreadingFactor" }},
+            
+        },
+        pit={
+            "id": pit_id,
+            "keep_alive": "1m",
+        },
+        sort=[
+            {"mqtt_time": {"order": "asc"}},
+            {"_score": {"order": "desc"}},
+        ],
+        search_after=[
+            datemin,
+            0
+        ],
+    )
+    
+    print(resp)
+            
+          
+          
 
     
+ 
+
 
 
 #delete the PIT
