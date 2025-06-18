e69c85699ed15ded888b115a010b9dc03ebbce73
Fabrice Théoleyre <fabrice.theoleyre@cnrs.fr>
2025-06-18 11:00:53 +0200

    renaming

--- analysis/duplicates.py
+++ analysis/duplicates.py
@@ -1,7 +1,7 @@
-""" inter packet time distribution analysis .
+""" Duplicates analysis .
 
-This scripts reads the distribution stored on the disk and
-analyzes them (i.e., plot some graphs)
+This scripts reads the packets stored on the disk (per flow distribution)
+and analyzes them (i.e., plot some graphs for all packets together)
 
 
 """
@@ -48,8 +48,8 @@ import random
 
 #logs
 import logging
-logger_allpackets = logging.getLogger('allpackets')
-logger_allpackets.setLevel(logging.INFO)
+logger = logging.getLogger('dups')
+logger.setLevel(logging.INFO)
 logging.basicConfig(stream=sys.stdout)
 
 
@@ -59,7 +59,7 @@ import extract_interpacket_distribution
 
 
 #parameters
-NB_PKTS_MIN = 50        # minimum number of packets for a given devAddr (else discarded) to compute the median value
+NB_PKTS_MIN = 5         # minimum number of packets for a given devAddr (else discarded) to compute the median value
 FNCT_DIFF_MAX = 100     # maximum average fnct_diff for a flow
 NB_PLOTS = 16           # number of plots for individual distributions
 
@@ -96,7 +96,7 @@ def plot_nbdups_time_heatmap(pd_flat_values):
     g.set(xlabel='Hour', ylabel='Number of duplicates')
 
     #save figure
-    g.figure.savefig("figures/allpackets_nbdups_time_heatmap.pdf")
+    g.figure.savefig("figures/duplicates_nbdups_time_heatmap.pdf")
 
    
    
@@ -125,7 +125,7 @@ def plot_nbdups_CDF(pd_flat_values):
         
     #save figure
     plt.tight_layout(pad=1.0, h_pad=None, w_pad=None)
-    g.figure.savefig("figures/allpackets_nbdups_cdf.pdf")
+    g.figure.savefig("figures/duplicates_nbdups_cdf.pdf")
     g.figure.clf()
          
 
@@ -145,17 +145,17 @@ if __name__ == "__main__":
  
     """
        
-    # ---- disk -----
+    # ---- disk - load data -----
     # load data that is on the disk (already read previously)
     pd_all_flows = extract_interpacket_distribution.load_from_disk(verbose=True)
     
-    logger_allpackets.info("Reading distributions from the disk....")
+    logger.info("Reading distributions from the disk....")
     if pd_all_flows.empty:
-        logger_allpackets.error("\t\t>0 devAddrs in the disk")
-        logger_allpackets.error("Nothing to read -> stop the analysis here")
+        logger.error("\t\t>0 devAddrs in the disk")
+        logger.error("Nothing to read -> stop the analysis here")
         exit(0)
     else:
-        logger_allpackets.debug("\t\t> "+ str(len(pd_all_flows)) + " devAddrs in the disk")
+        logger.debug("\t\t> "+ str(len(pd_all_flows)) + " devAddrs in the disk")
    
 
 
@@ -166,27 +166,32 @@ if __name__ == "__main__":
     nb_records_unfiltered = len(pd_all_flows)
     pd_all_flows = pd_all_flows[pd_all_flows['nb_pkts'] >= NB_PKTS_MIN]
     nb_records_minpkts = len(pd_all_flows)
-    logger_allpackets.info("\t\t> removed "+ str(nb_records_unfiltered - nb_records_minpkts) + " flows without enough packets (<" + str(NB_PKTS_MIN) + ")" )
+    logger.info("\t\t> removed "+ str(nb_records_unfiltered - nb_records_minpkts) + " flows without enough packets (<" + str(NB_PKTS_MIN) + ")" )
     
     #-- MAX FNCT DIFF
     pd_all_flows = pd_all_flows[pd_all_flows['mean_fCnt_diff'] <= FNCT_DIFF_MAX]
     nb_records_maxfnctdiff = len(pd_all_flows)
-    logger_allpackets.info("\t\t> removed "+ str(nb_records_minpkts - nb_records_maxfnctdiff) + " flows with a too high fnctdiff (>=" + str(FNCT_DIFF_MAX) + ")" )
-    logger_allpackets.info("\t\t> "+ str(len(pd_all_flows)) + " flows to process")
+    logger.info("\t\t> removed "+ str(nb_records_minpkts - nb_records_maxfnctdiff) + " flows with a too high fnctdiff (>=" + str(FNCT_DIFF_MAX) + ")" )
+    logger.info("\t\t> "+ str(len(pd_all_flows)) + " flows to process")
 
 
+
+    # --- Analysis ---
 
     
     # flat values for all the flows (a single dataframe with all the packets)
     pd_distrib = []
     for devAddr in pd_all_flows['devAddr']:
         pd_distrib = extract_interpacket_distribution.load_distribs_forDevAddr_from_disk(pd_all_flows, devAddr, pd_distrib)
+    #transform the array into a dataframe
     pd_flat_values = pd.concat(pd_distrib)
-
 
     # time distribution of the packet losses
     #plot_nbdups_time_heatmap(pd_flat_values)
         
     # CDF nb_duplicates
     plot_nbdups_CDF(pd_flat_values['nb_duplicates'])
+    
+    
+    # addresses
 
