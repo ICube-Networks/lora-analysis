6dba8f56305ec6521bd786b173d42b51db56ae20
Fabrice Théoleyre <fabrice.theoleyre@cnrs.fr>
2025-06-18 16:46:24 +0200

    operators distribution
    nb of packets per operator

--- analysis/allpackets.py
+++ analysis/allpackets.py
@@ -58,7 +58,38 @@ logging.basicConfig(stream=sys.stdout)
 # --------------------------------------------------------
 
 
-   
+           
+def plot_operators_cdf(devaddr_df):
+    """ Pairplot SF and PRR
+        
+    :param distribution: a pandas dataframe with the flows 
+    
+    """
+    
+    print(devaddr_df)
+    
+    sns.set()
+    sns.set_theme()
+    #sns.set(rc={"figure.figsize":(11, 5)})
+    #sns.color_palette("colorblind")
+    sns.set_context("notebook", font_scale=0.6)
+    
+    g = sns.catplot(
+        data = devaddr_df,
+        y = 'operator',
+        x = 'doc_count',
+        log_scale =  (True, False),
+        aspect = 0.75,
+        margin_titles = True,
+    )
+    g.set(ylabel='Operators', xlabel='Number of packets')
+       
+        
+    #save figure
+    #plt.tight_layout(pad=1.0, h_pad=None, w_pad=None)
+    g.figure.savefig("figures/allpackets_operators.pdf")
+    g.figure.clf()
+         
 
 
 # --------------------------------------------------------
@@ -78,4 +109,10 @@ if __name__ == "__main__":
     devaddr_df = extract_interpacket_distribution.es_query_get_devAddr()
     devaddr_df['operator'] = devaddr_df['devAddr'].apply(lambda x: lorawan_operators.find_operators(operators, x))
     
-    print(devaddr_df)
+    #shorten this specific operator (too long string)
+    devaddr_df.replace('Shenzhen Tencent Computer Systems Company Limited', 'Shenzhen Tencent', inplace = True)
+    
+    #regroup by operator, removing the devAddr field
+    operators_df = devaddr_df.groupby('operator')['doc_count'].sum().reset_index().sort_values(by=['doc_count'], ascending=False).reset_index(drop=True)
+
+    plot_operators_cdf(operators_df)
